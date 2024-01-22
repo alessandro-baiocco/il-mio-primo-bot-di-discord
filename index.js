1f2c15ec1f28d07d5521bf52dbb27595ae3629b1
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-01-22 19:10:04 +0100

    creato il primo commando

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
 require("dotenv").config();
 
 const token = process.env.TOKEN_BOT;
 const client = new Client({
-  intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.DirectMessages],
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Channel],
 });
 
+const modList = ["mod 1", "mod 2", "mod 3"];
+
 client.login(token);
+
+client.on("ready", () => {
+  console.log("Bot online");
+});
+client.on("messageCreate", (message) => {
+  const command = message.content.split(" ")[0];
+  console.log(command, message.content);
+  switch (command) {
+    case "!modList": {
+      console.log("ecco tutte le mod:");
+      modList.forEach((mod, i) => console.log(`${i + 1}| ${modList[i]}`));
+      break;
+    }
+    case "!addMod": {
+      console.log("e va bene aggiorno la lavagna");
+      break;
+    }
+    case "!remMod": {
+      console.log("va bene ma quanto mi fate lavorare però");
+      break;
+    }
+  }
+});
