d81e745345c4ad7f9d7e638bf832c8fd381c82c5
alessandro baiocco <baioccoalessandro97@gmail.com>
2024-01-23 18:27:46 +0100

    aggiunta feature nuove mod e controlli

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const client = new Client({
   partials: [Partials.Channel],
 });
 
-const modList = ["mod 1", "mod 2", "mod 3"];
+const oldMods = ["mod 1", "mod 2", "mod 3", "new 1", "new 2", "new 3"];
+let newMods = ["new 1", "new 2", "new 3"];
 
 client.login(token);
 
@@ -25,21 +26,51 @@ client.on("messageCreate", (message) => {
       break;
     }
     case "!modList": {
-      message.channel.send("ecco tutte le mod:");
-      let stringModList = "";
-      modList.forEach((mod, i) => (stringModList += `${i + 1}| ${modList[i]} \n`));
-      message.channel.send(stringModList);
+      if (oldMods.length > 0) {
+        message.channel.send("ecco tutte le mod:");
+        let stringModList = "";
+        oldMods.forEach((mod, i) => (stringModList += `${i + 1}| ${oldMods[i]} \n`));
+        if (newMods.length > 0) {
+          stringModList += "----------mod nuove------------------\n";
+          newMods.forEach((mod, i) => (stringModList += `${i + 1}| ${newMods[i]} \n`));
+        } else {
+          stringModList += "----------nessuna mod nuova------------------\n";
+        }
+        message.channel.send(stringModList);
+      } else {
+        message.channel.send("non c'e nessuna mod");
+      }
       break;
     }
     case "!addMod": {
       message.channel.send("e va bene aggiorno la lavagna");
-      modList.push(content.slice(1).toString().replace(",", " "));
+      const mod = content.splice(1).toString().replace(",", " ");
+      if (mod !== "") {
+        oldMods.push(mod);
+        newMods.push(mod);
+      } else {
+        message.channel.send("aspetta, ma non posso mettere " + mod + " è una stringa vuota");
+      }
+
       break;
     }
     case "!remMod": {
       message.channel.send("va bene ma quanto mi fate lavorare però");
       const index = content.slice(1) - 1;
-      modList.splice(index, 1);
+      console.log(index);
+      if (oldMods.length > index && index !== NaN) {
+        newMods.splice(newMods.indexOf(oldMods[index]), 1);
+        oldMods.splice(index, 1);
+      } else if (isNaN(index)) {
+        message.channel.send("mi hai dato del testo? MA A ME SERVE UN NUMERO DA 1 A " + oldMods.length);
+      } else {
+        message.channel.send("ok lo rimuovo ASPETTA ma lista non è cosi lunga arriva massimo a " + oldMods.length);
+      }
+      break;
+    }
+    case "!clearMod": {
+      message.channel.send("ok rimuovo le mod nuove, peccato");
+      newMods = [];
       break;
     }
   }
